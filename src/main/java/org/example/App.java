479991c7d241f580868283238a798d387d8a1608
Daniel King <danielking4684@gmail.com>
2024-01-31 12:50:35 -0500

    changed to accept longer form expressions, needs debugging

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,26 +1,77 @@
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        Input userInput = new Input();
-        
+    public static void main(String[] args) {
+        Input input = new Input();
+        String operationString = input.getInput();
+        List<String> equations = Arrays.asList(
+            operationString    
+            //"(5 + 3) * 2"
+        );
+        for(String e: equations)
+            System.out.println(solveEquation(e));
+    }
 
-        System.out.println();
-        System.out.println("Welcome to the Calculator app.");
-        System.out.println();
+    private static int solveEquation(String e) {
 
-        // get operation and reference variables
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
 
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('+', 1);
+        map.put('-', 1);
+        map.put('*', 2);
+        map.put('/', 2);
 
-        int input1 = userInput.getInput1();
-        String opInput = userInput.getOpInput();
-        int input2 = userInput.getInput2();
+        for (char ch : e.toCharArray()) {
 
-        Calculate calc = new Calculate(input1, input2, opInput);
-        calc.mathCalc();
-        calc.printResult();
-        System.out.println();
-        
+            if(Character.isDigit(ch)){
+                operands.push(ch-'0');
+            }
 
-        
-        // calculate
+            else if (ch == '(')
+                operators.push(ch);
+
+            else if (ch == ')') {
+                char c = operators.pop();
+                while (c != '(') {
+                    int n2 = operands.pop();
+                    int n1 = operands.pop();
+                    operands.push(operation(n1, n2, c));
+                    c = operators.pop();
+                }
+            }
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+
+                while (!operators.isEmpty() && operators.peek() != '(' && map.get(ch) <= map.get(operators.peek())) {
+                    int n2 = operands.pop();
+                    int n1 = operands.pop();
+                    operands.push(operation(n1, n2, operators.pop()));
+                }
+                operators.push(ch);
+            }
+        }
+
+        while(!operators.isEmpty()){
+            int n2 = operands.pop();
+            int n1 = operands.pop();
+            char ch = operators.pop();
+            operands.push(operation(n1, n2, ch));
+        }
+        return operands.pop();
+    }
+
+    private static int operation(int a, int b, char op) {
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return 0;
     }
 }
--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -1,31 +1,14 @@
 import java.util.Scanner;
 
 public class Input {
-    //String input1;
-    //String input2;
-    int input1;
-    int input2;
-    String opInput;
+    
+    String input;
     Scanner scanner = new Scanner(System.in);
 
-    public int getInput1() {
-        System.out.print("Please enter your first number:  ");
-        input1 = scanner.nextInt();
+    public String getInput() {
+        System.out.println("Please enter your expression:  ");
+        input = scanner.next();
         System.out.println();
-        return input1;
-    }
-    public String getOpInput() {
-        System.out.println("What operation would you like to perform?");
-        System.out.print("Options are: + - * / % . Enter here:  ");
-        scanner.nextLine();
-        opInput = scanner.nextLine();
-        System.out.println();
-        return opInput;
-    }
-    public int getInput2() {
-        System.out.print("Please enter your second number:  ");
-        input2 = scanner.nextInt();
-        System.out.println();
-        return input2;
+        return input;
     }
 }
